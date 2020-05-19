a3f23070969b88c6ff2a6288716b7b096189dc83
marrob <marrob50hz@gmail.com>
2020-05-19 11:26:54 +0200

    Added Setup Project

--- Konvolucio.MDFU200325/AppConstants.cs
+++ Konvolucio.MDFU200325/AppConstants.cs
@@ -6,7 +6,7 @@
         public const string ValueNotAvailable2 = "n/a";
         public const string InvalidFlieNameChar = "A file name can't contain any of flowing characters:";
         public const string SoftwareCustomer = "Konvolúció Bt.";
-        public const string SoftwareTitle = "MDFU200325 - MATL Device Firmware Updater CLIENT";
+        public const string SoftwareTitle = "MDFU200325 - MATL Device Firmware Upgrade Tool";
         public const string GenericTimestampFormat = "yyyy.MM.dd HH:mm:ss";
         public const string FileNameTimestampFormat = "yyMMdd_HHmmss";
         public const string FileFilter = "Binary File(*.bin)|*.bin";
